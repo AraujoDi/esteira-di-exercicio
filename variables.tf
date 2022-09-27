7e16cb8baaa27f59079d0ae73742adbfbf3b4ac6
AraujoDi <diaraujo_@hotmail.com>
2022-09-26 21:42:34 -0300

    Primeiro Commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# Criando variavaies
+
+variable "base_bucket_name" {
+  default = "diego-datalaketf"
+}
+
+variable "ambiente" {
+  default = "prod"
+}
+
+variable "numero_conta" {
+  default = "853007510052"
+}
